daf2c6cc4f90184c9c4d8f85aa3603c6e06786b2
MSagGik <108148690+MSagGik@users.noreply.github.com>
2022-08-08 12:58:50 +0300

    Update TestSpring.java

--- src/main/java/ru/msaggik/spring/TestSpring.java
+++ src/main/java/ru/msaggik/spring/TestSpring.java
@@ -1,1 +1,18 @@
-package ru.msaggik.spring;import org.springframework.context.support.ClassPathXmlApplicationContext;public class TestSpring {    public static void main(String[] args) {        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(                "applicationContext.xml"        );        //Music music = context.getBean("musicBean", Music.class);        // внедрение зависимостей        //MusicPlayer musicPlayer = new MusicPlayer(music);        // внедрение зависимостей через конструктор с использованием XML        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);        musicPlayer.playMusic();        context.close();    }}
+package ru.msaggik.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+        //Music music = context.getBean("musicBean", Music.class);
+        // внедрение зависимостей
+        //MusicPlayer musicPlayer = new MusicPlayer(music);
+        // внедрение зависимостей через конструктор с использованием XML
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
+        context.close();
+    }
+}
